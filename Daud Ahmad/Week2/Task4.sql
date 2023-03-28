0d06060743f538145d81b2a847659a4f028495d9
Daud Ahmad <73556387+DaudAhmad0303@users.noreply.github.com>
2023-03-28 19:52:03 +0500

    Added Week 2 - Task 3 and Task 4

---
+++ Daud Ahmad/Week2/Task4.sql
@@ -0,0 +1,8 @@
+-- To Create a Database
+--Create DATABASE university;
+
+--To Alter the Database name
+--Alter database MyUniversity Modify Name = university;
+
+--To delete a database 
+--Drop database university;
